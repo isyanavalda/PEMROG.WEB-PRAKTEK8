ed8745a6713dc1f30206712ba629edbf163db33c
isyanavalda <100085573+isyanavalda@users.noreply.github.com>
2022-05-18 15:31:07 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+// Konfigurasi Database
+$host="localhost";
+$user="root";
+$password="";
+$database="tugasform";
+// Perintah php untuk akses ke Database
+$koneksi=mysqli_connect($host, $user, $password, $database);
+?>
